4ba6271ec34a5f213ff00a668d1792f1b8b0f59c
AllLook <102340843+AllLook@users.noreply.github.com>
2023-08-25 11:34:58 +0300

    Update apps.py

--- myapp_1/apps.py
+++ myapp_1/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class Myapp1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Django_Project_1.myapp_1'
+    name = 'myapp_1'
